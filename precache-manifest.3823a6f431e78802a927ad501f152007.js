af28400bac659b138bda0e568baa8c0a82879285
Filip Pie <filipiusz@gmail.com>
2020-03-02 23:31:09 +0100

    Updates

---
+++ precache-manifest.3823a6f431e78802a927ad501f152007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20164e78a8ca8e87f27cb555babcfde5",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "d098c3491d64bec2637e",
+    "url": "/counter-app/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "99c026b3cddbc38e47b5",
+    "url": "/counter-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d098c3491d64bec2637e",
+    "url": "/counter-app/static/js/2.235c4452.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/counter-app/static/js/2.235c4452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c026b3cddbc38e47b5",
+    "url": "/counter-app/static/js/main.27858555.chunk.js"
+  },
+  {
+    "revision": "8083ab991f14a12e63e1",
+    "url": "/counter-app/static/js/runtime-main.8a07ac22.js"
+  }
+]);
